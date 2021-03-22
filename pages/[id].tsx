81c028175dcc2778ecc8a6966a88ea2ba601a708
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-23 06:15:35 +0900

    add ogp tag

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -36,6 +36,14 @@ export default function RecipePageProps(props: RecipePageProps) {
       <Head>
         <title>{props.title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:description" content={props.description} />
+        <meta property="og:type" content="article"/>
+        <meta property="og:article:author" content={props.author.user_name}/>
+        <meta property="og:article:published_time" content={props.published_at}/>
+        <meta property="og:article:modified_time" content={props.published_at}/>
+        <meta property="og:article:section" content="Cooking"/>
+        <meta property="og:image" content={props.image_url ? props.image_url : ""} />
       </Head>
       <header>
         <h2 className="bg-gray-300 p-2 text-xl font-bold">
