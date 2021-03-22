8d1948b870affb12f0b3c9f02aa5f961be1de25e
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-23 02:47:17 +0900

    APIでページネーションしてくれるの知らなかった
    バックエンドじゃないとページネーション厳しいのはそれはそう

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,24 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { MdSearch } from 'react-icons/md';
-import { getRecipes, Recipe } from '../lib/recipe';
+import { Recipe } from '../lib/recipe';
 import Heading from '../components/heading';
 
-const COLUMN_MAX = 4;
-
-type HomeProps = {
+type PageInfo = {
   recipes: Recipe[];
   page: number;
-  totalPage: number;
   search: string;
+  nextExists: boolean;
+  prevExists: boolean;
+};
+
+type HomeProps = {
+  pageInfo: PageInfo;
 };
 
 export default function Home(props: HomeProps) {
-  const { search, page, recipes, totalPage } = props;
+  const { pageInfo } = props;
+  const { search, page, recipes, nextExists, prevExists } = pageInfo;
   const [searchWord, setSearchWord] = useState(search);
   const genPageQuery = (p: number) => (p === 0 ? '' : `page=${p}`);
   const genSearchQuery = (s: string) => (s === '' ? '' : `search=${s}`);
@@ -53,25 +57,34 @@ export default function Home(props: HomeProps) {
         ))}
       </main>
       <footer>
-        {page > 0 ? <Link href={`/${genQuery(page - 1, search)}`}>前のページ</Link> : null}
-        {page + 1 < totalPage ? <Link href={`/${genQuery(page + 1, search)}`}>次のページ</Link> : null}
+        {prevExists ? <Link href={`/${genQuery(page - 1, search)}`}>前のページ</Link> : null}
+        {nextExists ? <Link href={`/${genQuery(page + 1, search)}`}>次のページ</Link> : null}
       </footer>
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const recipes = await getRecipes();
-  const totalPage = Math.ceil(recipes.length / COLUMN_MAX);
-  const pageRaw = Math.min(Math.max(Number(context.query.page), 0), totalPage - 1);
-  const page = Number.isNaN(pageRaw) ? 0 : pageRaw;
+  const pageRaw = Number(context.query.page);
+  const page = Number.isNaN(pageRaw) ? 1 : Math.max(pageRaw, 1);
   const search = context.query.search === undefined ? '' : context.params?.search;
+  const api_res = await fetch(`https://internship-recipe-api.ckpd.co/recipes?page=${page}`, {
+    method: 'GET',
+    headers: new Headers({
+      'X-Api-Key': process.env.COOKPAD_API_KEY as string,
+    }),
+  });
+  const res = await api_res.json();
+  console.log(res);
   return {
     props: {
-      recipes: recipes.slice(page * COLUMN_MAX, Math.min(recipes.length, (page + 1) * COLUMN_MAX)),
-      page,
-      search,
-      totalPage,
+      pageInfo: {
+        page,
+        search,
+        recipes: res.recipes,
+        nextExists: res.links.next !== undefined,
+        prevExists: res.links.prev !== undefined,
+      },
     },
   };
 };
