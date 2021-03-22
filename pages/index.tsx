c558ecfa18bb0a057c11e32110d8ad36e544810f
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-23 01:57:05 +0900

    SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { MdSearch } from 'react-icons/md';
 import { getRecipes, Recipe } from '../lib/recipe';
 import Heading from '../components/heading';
 
 const COLUMN_MAX = 4;
 
-export default function Home() {
-  const router = useRouter();
-  const [searchWordOfPage, setSearchWordOfPage] = useState('');
-  const [searchWord, setSearchWord] = useState('');
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [page, setPage] = useState(0);
-  const [totalPageN, setTotalPageN] = useState(0);
-  useEffect(() => {
-    (async () => {
-      const fetchedRecipes = await getRecipes();
-      const pageStr = router.query.page;
-      const pageOrNaN = pageStr && typeof pageStr === 'string' ? parseInt(pageStr, 10) : 0;
-      const parsedPage = Number.isNaN(pageOrNaN) ? 0 : pageOrNaN;
-      const parsedSearchWordOfPage =
-        router.query.search && typeof router.query.search === 'string' ? router.query.search : '';
-      setSearchWordOfPage(parsedSearchWordOfPage);
-      setSearchWord(parsedSearchWordOfPage);
-      setPage(parsedPage);
-      setTotalPageN(Math.ceil(fetchedRecipes.length / COLUMN_MAX));
-      setRecipes(fetchedRecipes.slice(parsedPage * COLUMN_MAX, (parsedPage + 1) * COLUMN_MAX));
-    })();
-  }, [router]);
+type HomeProps = {
+  recipes: Recipe[];
+  page: number;
+  totalPage: number;
+  search: string;
+};
+
+export default function Home(props: HomeProps) {
+  const { search, page, recipes, totalPage } = props;
+  const [searchWord, setSearchWord] = useState(search);
   const genPageQuery = (p: number) => (p === 0 ? '' : `page=${p}`);
   const genSearchQuery = (s: string) => (s === '' ? '' : `search=${s}`);
   const genQuery = (p: number, s: string): string => {
@@ -65,9 +53,27 @@ export default function Home() {
         ))}
       </main>
       <footer>
-        {page > 0 ? <Link href={`/${genQuery(page - 1, searchWordOfPage)}`}>前のページ</Link> : null}
-        {page + 1 < totalPageN ? <Link href={`/${genQuery(page + 1, searchWordOfPage)}`}>次のページ</Link> : null}
+        {page > 0 ? <Link href={`/${genQuery(page - 1, search)}`}>前のページ</Link> : null}
+        {page + 1 < totalPage ? (
+          <Link href={`/${genQuery(page + 1, search)}`}>次のページ</Link>
+        ) : null}
       </footer>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const recipes = await getRecipes();
+  const totalPage = Math.ceil(recipes.length / COLUMN_MAX);
+  const pageRaw = Math.min(Math.max(Number(context.query.page), 0), totalPage - 1);
+  const page = Number.isNaN(pageRaw) ? 0 : pageRaw;
+  const search = context.query.search === undefined ? '' : context.params?.search;
+  return {
+    props: {
+      recipes: recipes.slice(page * COLUMN_MAX, Math.min(recipes.length, (page + 1) * COLUMN_MAX)),
+      page,
+      search,
+      totalPage,
+    },
+  };
+};
