e7eec21f748bf18bf94ea7299672d2281dbce7fb
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-24 03:04:04 +0900

    integrate graphql schema

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,39 +1,10 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 import { gql } from 'apollo-server-micro';
+import { readFileSync } from 'fs';
+const schema = readFileSync('graphql/schema.graphql', 'utf8');
 
-export const typeDefs = gql`
-  type Author {
-    user_name: String!
-  }
-
-  type Ingredient {
-    name: String!
-    quantity: String!
-  }
-
-  type Recipe {
-    id: ID!
-    title: String!
-    description: String!
-    image_url: String
-    author: Author!
-    published_at: String!
-    steps: [String!]!
-    ingredients: [Ingredient!]!
-    related_recipes: [Int!]!
-  }
-
-  type RecipePage {
-    recipes: [Recipe!]!
-    has_next: Boolean!
-    has_prev: Boolean!
-  }
-
-  type Query {
-    recipes(page: Int): RecipePage!
-  }
-`;
+export const typeDefs = gql(schema);
 
 class CookpadAPI extends RESTDataSource {
   constructor() {
