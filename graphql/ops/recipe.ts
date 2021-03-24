ebea26a8afe0ade52a86ef1698055b8c14289eeb
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-24 13:04:54 +0900

    fmt

--- graphql/ops/recipe.ts
+++ graphql/ops/recipe.ts
@@ -1,19 +1,20 @@
 import gql from 'graphql-tag';
 
 export default gql`
-query Recipe ($id: ID!) {
-  recipe(id: $id) {
-    id
-    title
-    author {
-      user_name
+  query Recipe($id: ID!) {
+    recipe(id: $id) {
+      id
+      title
+      author {
+        user_name
+      }
+      image_url
+      description
+      ingredients {
+        name
+        quantity
+      }
+      steps
     }
-    image_url
-    description
-    ingredients {
-      name
-      quantity
-    }
-    steps
   }
-}`;
+`;
